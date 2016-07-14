3c51ae1b3d1998e3ba1b57254a07da69f0d923ab
sailakshmiv <lakshmi.saiv99@gmail.com>
2016-07-14 13:06:25 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -4,16 +4,11 @@ module.exports = function(sequelize, DataTypes) {
   var Task = sequelize.define("Task", {
     title: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        Task.belongsTo(models.User, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        });
-      }
-    }
+   classMethods: {
+  associate: function(models) {
+    Task.belongsTo(models.User);
+  }
+}
   });
 
   return Task;
